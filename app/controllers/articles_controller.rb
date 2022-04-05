8a2f94ab470b323930fef7869cb5bb939d069c6f
sergio <sergiocg.calvo@gmail.com>
2022-04-05 18:56:17 +0200

    enviar datos al controlador - formulario

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,5 +10,19 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id]);
     end
 
+    # GET /articles/new
+    def new
+        #Article.new --> no guarda nada en la base de datos. Solo lo guarda en memoria
+        @article = Article.new
+    end
+
+    # POST /articles
+    def create
+        @article = Article.new(title: params[:article][:title], 
+                                body: params[:article][:body])
+        @article.save
+        redirect_to @article
+    end
+
 
 end
